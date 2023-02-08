f5d826328b593edb8f6f831f01d66378b69a70b4
Jarne Van Aerde <jarne.vanaerde@protime.eu>
2023-02-08 08:40:07 +0100

    Added benchmark project

---
+++ LoggingBestPractices.Benchmarks/LoggingBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingBestPractices.Benchmarks;
+
+public class LoggingBenchmarks
+{
+    private const string LogMessageWithParameters = "This is a log message with parameters {0}, {1}";
+    private const string LogMessage = "This is a log message";
+
+    private readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole().SetMinimumLevel(LogLevel.Warning);
+    });
+
+    private readonly ILogger<LoggingBenchmarks> _logger;
+
+    public LoggingBenchmarks()
+    {
+        _logger = new Logger<LoggingBenchmarks>(_loggerFactory);
+    }
+
+    [Benchmark]
+    public void Log_WithoutIf()
+    {
+        _logger.LogInformation(LogMessage);
+    }
+        
+    [Benchmark]
+    public void Log_WithIf()
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(LogMessage);
+        }
+    }
+}
