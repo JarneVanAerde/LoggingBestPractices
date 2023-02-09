3bb32af1ea682b31f15da2f3854bb8237acfc481
Jarne Van Aerde <jarne.vanaerde@protime.eu>
2023-02-09 08:26:08 +0100

    added some more examples

---
+++ LoggingBestPractices.MemoryProfiling/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole().SetMinimumLevel(LogLevel.Warning);
+});
+
+ILogger logger = new Logger<Program>(loggerFactory);
+
+for (var i = 0; i < 100_000_000; i++)
+{
+    logger.LogInformation("Random number {RandomNumber}", Random.Shared.Next());
+    
+    // This is better
+    // if (logger.IsEnabled(LogLevel.Information))
+    // {
+    //     logger.LogInformation("Random number {RandomNumber}", Random.Shared.Next());
+    // }
+    
+    // this is even worse!
+    //logger.LogInformation($"Random number {Random.Shared.Next()}");
+}
