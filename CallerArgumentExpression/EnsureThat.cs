ed068195f1fec1e7cb03e6d5d5bf7a8ff9b05ee4
Jarne Van Aerde <jarne.vanaerde@protime.eu>
2023-02-23 20:48:17 +0100

    added the callerargumentextensions project

---
+++ CallerArgumentExpression/EnsureThat.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CallerArgumentExpression;
+
+public static class EnsureThat
+{
+    public static void ItIsEmpty<T>(IEnumerable<T> value)
+    {
+        if (value.Any())
+        {
+            throw new ArgumentException("Enumerable is not empty", nameof(value));
+        }
+    }
+    
+    public static void ItIsNotEmpty<T>(IEnumerable<T> value,
+        [CallerArgumentExpression("value")] string message = "")
+    {
+        if (!value.Any())
+        {
+            throw new ArgumentException("Enumerable is empty", message);
+        }
+    }
+    
+    public static void ItIsNotNull<T>(T value)
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value));
+        }
+    }
+    
+    public static void ItIsTrue(bool value)
+    {
+        if (!value)
+        {
+            throw new ArgumentException("Value is false", nameof(value));
+        }
+    }
+}
---
+++ CallerArgumentExpression/Program.cs
@@ -0,0 +1,9 @@
+﻿using CallerArgumentExpression;
+
+var array = new[] { 77, 568 };
+    
+// Debug.Assert(array.Length == 0, "array.Length == 0");
+// CallerArgumentExpression is as performant as if you were to pass it by text yourself
+// IL-vierwer misschien handig?
+
+EnsureThat.ItIsNotEmpty(array);
